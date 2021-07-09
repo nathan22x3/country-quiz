a239afb7c99aeccc7ad7cdec8578f317820db2d1
Nathan Nguyen <67423224+nathan22x3@users.noreply.github.com>
2021-07-09 18:04:39 +0700

    Add footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Redirect } from 'react-router-dom';
 /** @jsxImportSource @emotion/react */
+import tw from 'twin.macro';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -14,11 +15,25 @@ const App = () => {
   }, []);
 
   return (
-    <div className='flex items-center w-screen min-h-screen px-7 py-20 bg-app bg-cover'>
+    <div className='relative flex items-center w-screen min-h-screen px-7 py-20 bg-app bg-cover'>
       <Router basename='/'>
         <Redirect exact path='/' to='/play' />
         <Quiz />
       </Router>
+      <footer
+        css={tw`absolute bottom-0 left-0 w-screen py-7 text-center text-sm font-normal font-montserrat text-white`}
+      >
+        created by{' '}
+        <a
+          href='http://github.com/nathan22x3'
+          target='_blank'
+          rel='noopener noreferrer'
+          css={tw`underline font-bold`}
+        >
+          nathan22x3
+        </a>{' '}
+        - devChallenges.io
+      </footer>
     </div>
   );
 };
